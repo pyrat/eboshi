d1d9ea5db4ea9e90560a1921071361e58c78d4d1
Micah Geisel <originofstorms@gmail.com>
2008-07-17 11:02:27 -0700

    show errors on production. tweaked migration

--- db/migrate/20080713031304_add_type_to_line_items.rb
+++ db/migrate/20080713031304_add_type_to_line_items.rb
@@ -1,8 +1,7 @@
 class AddTypeToLineItems < ActiveRecord::Migration
   def self.up
   	add_column :line_items, :type, :string
-  	LineItem.update_all "type = 'Work'", "invoice_id IS NOT NULL"
-  	LineItem.update_all "type = 'ToDo'", "invoice_id IS NULL"
+  	LineItem.update_all "type = 'Work'"
   end
 
   def self.down
