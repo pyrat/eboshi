c741b4e9e28501b21b89f61c26ddffc1cfc0d0d3
Micah Geisel <originofstorms@gmail.com>
2008-07-18 02:11:13 -0700

    testing git rm

--- db/schema.rb
+++ db/schema.rb
@@ -26,22 +26,22 @@ ActiveRecord::Schema.define(:version => 20080717191710) do
   end
 
   create_table "invoices", :force => true do |t|
-    t.integer  "client_id",    :limit => 11
+    t.integer  "client_id"
     t.datetime "date"
     t.datetime "paid"
     t.string   "project_name"
   end
 
   create_table "line_items", :force => true do |t|
-    t.integer  "client_id",  :limit => 11
-    t.integer  "user_id",    :limit => 11
+    t.integer  "client_id"
+    t.integer  "user_id"
     t.datetime "start"
     t.datetime "finish"
-    t.decimal  "rate",                     :precision => 10, :scale => 2
+    t.decimal  "rate"
     t.text     "notes"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.integer  "invoice_id", :limit => 11
+    t.integer  "invoice_id"
     t.string   "type"
   end
 
@@ -54,7 +54,7 @@ ActiveRecord::Schema.define(:version => 20080717191710) do
     t.datetime "updated_at"
     t.string   "remember_token"
     t.datetime "remember_token_expires_at"
-    t.decimal  "rate",                                    :precision => 10, :scale => 2
+    t.decimal  "rate"
   end
 
 end
