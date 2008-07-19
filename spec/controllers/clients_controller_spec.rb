a87a6abf4486106af030426d433cd353e4d9677c
Micah Geisel <originofstorms@gmail.com>
2008-07-18 21:30:32 -0700

    added controller/view specs.

---
+++ spec/controllers/clients_controller_spec.rb
@@ -0,0 +1,35 @@
+require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
+
+describe ClientsController do
+  integrate_views
+  
+	before(:each) do
+		controller.stub!(:authenticate_or_request_with_http_basic).and_return(true)
+		@client = clients(:NANETS)
+	end
+
+  describe "should not error out" do
+    it "on index" do
+      get :index
+    end
+    it "on show" do
+      get :show, :id => @client.id
+    end
+    it "on edit" do
+      get :edit, :id => @client.id
+    end
+    it "on new" do
+      get :new
+    end
+    it "on update" do
+      put :update, :id => @client.id, :client => @client.attributes
+    end
+    it "on destroy" do
+      delete :destroy, :id => @client.id
+    end
+    it "on create" do
+      post :create, :client => @client.attributes
+    end
+  end
+
+end
---
+++ spec/controllers/line_items_controller_spec.rb
@@ -0,0 +1,10 @@
+require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
+
+describe LineItemsController do
+
+  #Delete these examples and add some real ones
+  it "should use LineItemsController" do
+    controller.should be_an_instance_of(LineItemsController)
+  end
+
+end
